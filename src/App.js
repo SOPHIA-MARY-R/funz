515e34eec0998deee4fb63d0055e6021087b2443
SOPHIA MARY R <soph20410.cs@rmkec.ac.in>
2023-01-06 13:04:28 +0530

    Resolved code smells

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 //export default allows you to rename the components at the time of import, so they are not enclosed within braces
 //export const strictly sticks with the component name, u should only use the exact name within braces
 import Navbar from './components/Navbar'
--- src/tabs/Movies.js
+++ src/tabs/Movies.js
@@ -2,8 +2,6 @@ import Carousels from "../components/Carousels";
 import './tabs.css'
 import ToggleTag from "../components/ToggleTag";
 import { LangData } from "../components/LangData";
-import Sidebar from "../components/Sidebar";
-import { SidebarData } from "../components/SidebarData";
 
 export const Movies =()=> {
     const toggle = LangData.map((item) => (
