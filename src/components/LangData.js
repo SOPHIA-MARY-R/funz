e8c907118dbdd9802ef5e802db83d4976a23afd5
Sophia Mary Rayappan <soph20410.cs@rmkec.ac.in>
2023-01-06 13:16:19 +0530

    Add files via upload

---
+++ src/components/LangData.js
@@ -0,0 +1,30 @@
+export const LangData =[
+    {
+        id: 1,
+        name: "Tamil"
+    },
+    {
+        id: 2,
+        name: "Telugu"
+    },
+    {
+        id: 3,
+        name: "English"
+    },
+    {
+        id: 4,
+        name: "Hindi"
+    },
+    {
+        id: 5,
+        name: "Bengali"
+    },
+    {
+        id: 6,
+        name: "Marathi"
+    },
+    {
+        id: 7,
+        name: "Malayalam"
+    }
+]
