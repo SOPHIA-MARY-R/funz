4f2137d65ddffab3f5c8315644cec9e3a0c9849f
SOPHIA MARY R <soph20410.cs@rmkec.ac.in>
2023-01-06 13:08:15 +0530

    Updated NavData

--- src/components/Navdata.js
+++ src/components/Navdata.js
@@ -1,55 +1,66 @@
 export const NavData = [
     {
+        id: 1,
         head: "Movies",
         link: "/movies",
         classname: "Movies"
     },
     {
+        id: 2,
         head: "Stream",
         link: "/stream",
         classname: "Stream"
     },
     {
+        id: 3,
         head: "Events",
         link: "/events",
         classname: "Events"
     },
     {
+        id: 4,
         head: "Plays",
         link: "/plays",
         classname: "Plays"
     },
     {
+        id: 5,
         head: "Sports",
         link: "/sports",
         classname: "Sports"
     },
     {
+        id: 6,
         head: "Activities",
         link: "/activities",
         classname: "Activities"
     },
     {
+        id: 7,
         head: "Buzz",
         link: "/buzz",
         classname: "Buzz"
     },
     {
+        id: 8,
         head: "GiftCards",
         link: "#",
         classname: "Gift"
     },
     {
+        id: 9,
         head: "Offers",
         link: "#",
         classname: "Offers"
     },
-    {
+    {   
+        id: 10,
         head: "Corporates",
         link: "#",
         classname: "Corporates"
     },
     {
+        id: 11,
         head: "ListYourShow",
         link: "#",
         classname: "List"
